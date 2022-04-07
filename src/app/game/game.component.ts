58d36107c1dc4cd16fdda645d028f5aaa29f062e
Mabu4 <max.budziat@gmx.de>
2022-04-07 10:45:27 +0200

    added played Cards, JSON Array push and slice with played and available cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -8,6 +8,7 @@ import { Game } from 'src/models/game';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  currentCard: string = '';
   game: Game;
 
   constructor() { 
@@ -19,12 +20,21 @@ export class GameComponent implements OnInit {
 
   newGame(){
     this.game = new Game();
-    console.log(this.game);
+    
   }
   
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if(!this.pickCardAnimation){
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+      console.log(this.game);
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 2000);
+    }
+    
   }
 
 }
